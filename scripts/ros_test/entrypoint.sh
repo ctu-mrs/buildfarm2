a48af8fea049cc5569ffb7ea4e736bf7484afd1c
Tomas Baca <Tomas Baca>
2025-10-18 09:55:10 +0200

    wip testing

--- scripts/ros_test/entrypoint.sh
+++ scripts/ros_test/entrypoint.sh
@@ -36,7 +36,7 @@ cd $WORKSPACE
 
 source $WORKSPACE/install/setup.bash
 
-colcon build --symlink-install --cmake-args -DENABLE_COVERAGE=true -DENABLE_TESTS=true
+colcon build --symlink-install --base-paths $WORKSPACE/src/$REPOSITORY_NAME --cmake-args -DENABLE_COVERAGE=true -DENABLE_TESTS=true
 
 source $WORKSPACE/install/setup.bash
 
